122407a31abcffdb65d3ba8d5d1d3d6371e53289
LuckbrjDev <246929449+LuckbrjDev@users.noreply.github.com>
2025-12-20 19:30:25 -0300

    Versão 2.0b (mensagem de avaliação)

--- script.js
+++ script.js
@@ -39,8 +39,9 @@ function renderClientes() {
       navigator.clipboard.writeText(
 `Olá, tudo bem? 😊
 Aqui é o "${c.tecnico}" da Soften.
-
-Aproveitando, a avaliação foi enviada para o e-mail "${c.email}".
+Só passando para confirmar se ficou alguma dúvida ou pendência do nosso último atendimento — posso te ajudar em algo mais?
+Aproveitando, percebi que a avaliação referente ao meu atendimento ainda está pendente. O formulário foi enviado para seu e-mail "${c.email}".
+Se puder dar uma olhadinha (inclusive no Spam), essa avaliaçao me ajuda muito!
 Obrigado pela colaboração! 💙`
       );
     };
